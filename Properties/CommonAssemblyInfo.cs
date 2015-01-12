dc7fbb52ae7bf94434dfe8860a415b87cec7b386
klinkby <m@kli.dk>
2015-01-12 21:35:59 +0100

    fix culture

--- Properties/CommonAssemblyInfo.cs
+++ Properties/CommonAssemblyInfo.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 #if DEBUG
 
 [assembly: AssemblyConfiguration("Debug")]
 #else
-
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
 [assembly: AssemblyCompany("Mads Klinkby")]
 [assembly: AssemblyProduct("Klinkby")]
-[assembly: AssemblyCopyright("Copyright © Mads Breusch Klinkby 2011-2014")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCopyright("Copyright © Mads Breusch Klinkby 2011-2015")]
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
